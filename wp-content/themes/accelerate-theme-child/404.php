a6b4a97bc04f78365ab5f8134889dabcbdb82eac
Rachel K <rachelmkamath@gmail.com>
2018-10-09 20:06:04 -0500

    working on Twitter feed

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the 404 page
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<section class="page-404">
+    <div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+			<h1><span>Oops!</span> It seems you've lost your way.</h1>
+            <img src="/accelerate/wp-content/uploads/2018/10/daniel-gonzalez-602501-unsplash-768x1365.jpg" alt="Lost woman looking at map" />
+            <h4>No worries, we'll get you back on track.</h4>
+            <p><a href="/accelerate">Go back home</a></p>
+        </div><!-- .main-content -->
+    </div><!-- #primary -->
+    </section>
+
+<?php get_footer(); ?>
